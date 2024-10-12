1666120b3919502197630f559cbfdaa201c2b429
Keith Owens Jr. <owenskei000@warren.k12.in.us>
2024-10-12 08:11:44 -0400

    Final commit for now

---
+++ index.js
@@ -0,0 +1,7 @@
+let mini = document.querySelector(".mini");
+let startInfo = document.querySelector(".simpleInfo");
+let btn = document.querySelector(".subBtn")
+
+btn.addEventListener("click", function(){
+    alert("Thank you")
+})
